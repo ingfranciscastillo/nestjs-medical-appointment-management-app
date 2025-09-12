99e9c32c1ae0c4dfd322dc6944284a6cf8c44f3f
Francis Miguel Castillo Cruz <franciscastillodev@proton.me>
2025-09-12 02:32:34 -0400

    Agrega archivo de configuración .env.example y actualiza eslint.config.mjs para incluir el plugin @darraghor/eslint-plugin-nestjs-typed. También se actualiza package.json y pnpm-lock.yaml para incluir la nueva dependencia.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import eslintNestjs from '@darraghor/eslint-plugin-nestjs-typed';
 
 export default tseslint.config(
   {
@@ -28,7 +29,8 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
     },
   },
+  eslintNestjs.configs.flatRecommended,
 );
